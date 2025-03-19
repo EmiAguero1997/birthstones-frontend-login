12c51df6755e5a90b66b5a08b382f735880f36c6
Emiliano Aguero <e.aguero@birthstones.app>
2025-03-19 18:10:54 -0300

    primeng custom theme added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import {MyPreset} from '../app/themes/perfil-theme';
 
 import { routes } from './app.routes';
 
@@ -12,7 +13,7 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideOAuthClient(), provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura
+            preset: MyPreset
         }
     })]
 };
