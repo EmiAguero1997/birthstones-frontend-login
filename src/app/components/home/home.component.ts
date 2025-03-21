b4eb51aa11251e4243a47e26c18d558c1a55732d
Emiliano Aguero <e.aguero@birthstones.app>
2025-03-21 19:02:22 -0300

    btn to call endpoint added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -13,5 +14,16 @@ export class HomeComponent {
     let identity = this.authSv.getIdentity();
     console.log('token: ', token);
     console.log('identity: ', identity);
+  }
+
+  callExampleEndpoint(){
+    this.authSv.tryEndpoint().subscribe(
+      response=>{
+        console.log('response: ',response);
+      },
+      error=>{
+        console.log('error: ', error);
+      }
+    )
   }
 }
